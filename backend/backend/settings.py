3836a9c273fc16a002bed039ab90551e96e6b5df
Jeong-Hyeon-Lee <hyeon00@ewhain.net>
2022-11-23 12:43:20 +0900

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -74,6 +74,16 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+DATABASES = {
+    'default' : {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': env('DATABASE_NAME'),
+        'USER': env('DATABASE_USER'),
+        'PASSWORD': env('DATABASE_PASSWORD'),
+        'HOST': env('DATABASE_HOST'),
+        'PORT': env('DATABASE_PORT'),
+    }
+}
 
 
 # Password validation
